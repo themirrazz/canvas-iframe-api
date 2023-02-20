ebc50e7da107259c65f0a1c1cd288a6d51ab5c35
themirrazz <89490659+themirrazz@users.noreply.github.com>
2023-02-20 15:23:51 -0800

    random UUID and LTI internals

--- index.js
+++ index.js
@@ -47,5 +47,65 @@
             },
         );
         return prom;
+    };
+    var __uuid_random_hex = function () {
+        return ['A','B','C','D','E','F','0','1','2','3','4','5','6','7','8','9'][Math.floor(Math.random()*16)];
+    };
+    var __uuid_random_uid = function () {
+        return (
+            __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + "-"
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + "-"
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + "-"
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + "-"
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+            + __uuid_random_hex()
+        );
     }
+    var __lti_send_callback = function (data, callback) {
+        var __uuid = __uuid_random_uid();
+        var __cbf = function (event) {
+            if(event.message_id && event.message_id == event.data.message_id) {
+                callback(event.data);
+                try {
+                    window.removeEventListener('message',__cbf)
+                } catch (error) {
+                    return null;
+                }
+            }
+        }
+        if(data.message_id && window.addEventListener) {
+            window.addEventListener('message',data.message_id)
+        }
+        parent.postMessage(data,'*');
+    };
 })();
